64ad1a79576f03b9589545614eabec684fcbeba9
Marat2626 <Marat-iskhakov555@yandex.ru>
2026-05-10 18:14:25 +0300

    Исправлена строка подключения к БД

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres@localhost:5432/cakeme_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres@localhost:5432/cakeme_db")
 
 
 engine = create_engine(DATABASE_URL)
